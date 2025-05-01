ba8df9a94cecac67489ff0bec05b2cab326993c9
Krishna . <krishnassss365@gmail.com>
2025-05-01 19:43:47 +0000

    makingPublicRepo

--- ai_model/config/db.py
+++ ai_model/config/db.py
@@ -7,7 +7,6 @@ def get_db():
     try:
         mongo_url = os.getenv("MONGODB_URI")
         mongo_client = pymongo.MongoClient(mongo_url)
-        # mongo_client = pymongo.MongoClient("mongodb+srv://adminamlgo:amlgogo@cluster0.0bcnc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
         db = mongo_client["form_db"]  
         print("MongoDB connected successfully!")
         return db
